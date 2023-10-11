9fcf434d7eb6df9f5d0ef11568079c4b772226f0
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-10-11 09:25:14 +0200

    features: add decoder for each Swap variable of interest and where types are fetched from decoded swap event

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,52 @@ mod etherscan;
 mod test_sig_match;
 
 use ethers::{
-    core::types::{Filter, Log, H160, I256},
+    core::types::{Filter, Log, H160, U256},
     providers::{Provider, Ws},
     prelude::*,
-    abi::{Abi, RawLog, EventExt},
+    abi::{Abi, RawLog, EventExt, Detokenize, Token},
     utils::keccak256,
 };
 use eyre::Result;
 use dotenv::dotenv;
 use serde::Deserialize;
 use std::collections::HashMap;
+
+// resources:
+// https://www.gakonst.com/ethers-rs/subscriptions/logs.html?highlight=abi#subscribing-to-logs
+// https://docs.infura.io/networks/ethereum/json-rpc-methods/eth_getlogs
+// https://www.gakonst.com/ethers-rs/subscriptions/multiple-subscriptions.html helpful for the long run
+
+
+fn print_decoded_params(decoded: &ethers::abi::Log, variable_names: &[&str]) {
+    for param in &decoded.params {
+        if variable_names.contains(&param.name.as_str()) {
+            match param.name.as_str() {
+                "amount0" | "amount1" => {
+                    if let Token::Int(value) = &param.value {
+                        if *value > U256::from(i128::MAX as u128) {
+                            let neg_value = U256::max_value() - *value + U256::one();
+                            println!("{}: -{}", param.name, neg_value);
+                        } else {
+                            println!("{}: {}", param.name, value.low_u128() as i128);
+                        }
+                    }
+                },
+                "sqrtPriceX96" | "liquidity" => {
+                    if let Token::Uint(value) = &param.value {
+                        println!("{}: {}", param.name, value.low_u128());
+                    }
+                },
+                "tick" => {
+                    if let Token::Int(value) = &param.value {
+                        println!("{}: {}", param.name, value.low_u64() as i32);
+                    }
+                },
+                _ => println!("Unknown parameter: {}", param.name)
+            }
+        }
+    }
+}
 
 
 /// process_log Processes a given Ethereum log entry using the provided ABI.
@@ -34,14 +70,19 @@ async fn process_log(log: Log, abi: &Abi) -> Result<(), Box<dyn std::error::Erro
     };
 
     let log_topic: H256 = log.topics[0];
-    let hex_string = "0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67";
-    let parsed_hex: H256 = hex_string.parse().expect("Failed to parse hexadecimal string");
 
+    // Create an empty HashMap to store the Keccak256 hash of event signatures as the key,
+    // and a tuple of event name and the event structure as the value.
     let mut event_map = HashMap::new();
+
+    // Iterate over each event in the ABI.
     for (event_name, events) in &abi.events {
         for event in events {
-            // event.abi_signature() and not event.signature() instead of /!\
-            // -> keccack256 and ethers::abi::EventExt::keccack256 from https://docs.rs/ethers/latest/ethers/abi/struct.Event.html
+            // /!\ We use event.abi_signature() instead of event.signature() here.
+            // The reason is that `event.signature()` provides a human-readable format,
+            // while `event.abi_signature()` provides the human-readable ABI signature
+            // format suitable for hashing to match Ethereum's log signature standard.
+            // https://docs.rs/ethers/latest/ethers/abi/struct.Event.html
             let event_signature_hash = keccak256(event.abi_signature().as_bytes());
             if event_map.contains_key(&event_signature_hash) {
                 println!("Duplicate hash detected for event: {}", event_name);
@@ -51,29 +92,31 @@ async fn process_log(log: Log, abi: &Abi) -> Result<(), Box<dyn std::error::Erro
     }
 
     let mut successfully_decoded = false;
+
+    // Iterate over each event signature hash in our map.
     for (hash, (event_name, event)) in &event_map {
+        // check if the event_name is equal to Swap
+        if event_name != "Swap" {
+            println!("Skipping event: {}", event_name);
+            continue;
+        }
+        // Check if the first topic of the log (which is the event signature) matches the current hash.
         if log_topic.as_bytes() == *hash {
+            // If the log's topic matches an event's signature, attempt to parse the raw log using the event's ABI details.
+            // If the parsing fails, it might be due to reasons like a mismatched or outdated ABI, corrupted log data,
+            // non-standard encoding, or other discrepancies between the log and the ABI definition.
             let result = event.parse_log(raw_log.clone()).map_err(|e| eyre::eyre!("Failed to decode event: {:?}", e));
 
             if let Ok(decoded) = result {
-                println!("Successfully decoded event: {event_name}");
-                let amount0_token = decoded.params.iter().find(|param| param.name == "amount0");
-                if let Some(token) = amount0_token {
-                    let amount0: U256 = token.value.clone().into_int().unwrap();
-                    println!("amount0: {}", amount0);
-                } else {
-                    println!("Could not find 'amount0' in decoded params.");
-                }
+                println!("\nSuccessfully decoded event: {event_name}");
 
-                let amount1_token = decoded.params.iter().find(|param| param.name == "amount1");
-                if let Some(token) = amount1_token {
-                    let amount1: U256 = token.value.clone().into_int().unwrap();
-                    println!("amount1: {}", amount1);
-                } else {
-                    println!("Could not find 'amount1' in decoded params.");
-                }
+                // TODO write to CSV
+                // TODO unit tests
+                let variable_names = ["amount0", "amount1", "sqrtPriceX96", "liquidity", "tick"];
+                print_decoded_params(&decoded, &variable_names);
 
                 successfully_decoded = true;
+                println!("{}", '\n');
                 break;  // We break here since we successfully decoded the event
             } else {
                 println!("Failed to decode event with signature: {}", event.signature());
@@ -81,7 +124,7 @@ async fn process_log(log: Log, abi: &Abi) -> Result<(), Box<dyn std::error::Erro
         }
 
         if !successfully_decoded {
-            println!("Failed to decode any event for this log.");
+            // println!("Failed to decode any event for this log.");
         }
     }
     Ok(())
